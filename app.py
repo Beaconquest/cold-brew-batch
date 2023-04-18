8f4ae20f5a4b00e766d169bb69d84848d6e1fcc3
Promise Mwenga <promise_mwenga@hotmail.com>
2023-04-18 14:18:58 -0300

    installed flask and Flask_SQLAlchemy, setup database, outined database models

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("Hello world!")
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# initiate the app
+app = Flask(__name__)
+
+# config the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICIATIONS'] = False
+db = SQLAlchemy(app)
+
+# database models
+class Batch(db.Model):
+    pass
+
+class Product(db.Model):
+    pass
+
+# Association table
+customer_order = db.Table('customer_order', )
+
+class Customer(db.Model):
+    pass
+
+class Order(db.Model):
+    pass
+
