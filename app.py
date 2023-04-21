fc3a0b806124d001297cfef038722d981b8569f8
Promise Mwenga <promise_mwenga@hotmail.com>
2023-04-21 10:54:48 -0300

    completed batch model

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 # database models
 class Batch(db.Model):
     """
-    A class to represent a batch of cold brew coffee, in the database.
+    A class to represent a batch information for cold brew coffee, in the database.
     
     Attributes
     __________
@@ -22,35 +22,15 @@ class Batch(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     coffee_name = db.Column(db.String(100), nullable=False )
-    batch_water_volume = db.Column(db.Float, nullable=False)
+    batch_type = db.Column(db.String(50), nullable=False)
+    batch_start_water_volume = db.Column(db.Float, nullable=False)
+    batch_end_water_volume = db.Column(db.Float, nullable=False)
+    batch_start_water_temperature = db.Column(db.Float, nullable=False)
+    batch_end_water_temperature = db.Column(db.Float, nullable=False)
+    batch_coffee_quantity = db.Column(db.Float, nullable=False)
     batch_start_date = db.Column(db.DateTime)
-    batch_end_date = db.Column(db.DateTime)
-
-
-class Product(db.Model):
-    """
-    A class to represent a product of cold brew coffee, in the database.
-    """
-    id = db.Column(db.Integer, primary_key=True)
-
-# Association table
-customer_order = db.Table('customer_order', )
-
-class Customer(db.Model):
-    """
-    A class to represent a customer or vendor of cold brew coffee, in the database.
-    """
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    address = db.Column(db.String(250), nullable=False)
-    city = db.Column(db.String(100), nullable=False)
-    postal_code = db.Column(db.String(5), nullable=False)
-    email = db.Column(db.String(250), nullable=False)
-
-
-class Order(db.Model):
-    """
-    A class to represent an order of cold brew coffee, in the database.
-    """
-    id = db.Column(db.Integer, primary_key=True)
+    batch_estimated_end_date = db.Column(db.DateTime)
+    batch_actual_end_date = db.Column(db.DateTime)
+    Batch_expiration_date = db.Column(db.DateTime)
+    batch_ph = db.Column(db.Float, nullable=False)
 
