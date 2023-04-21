b8805e9600d4216919b35225bd4a0bad3dc9bb41
Promise Mwenga <promise_mwenga@hotmail.com>
2023-04-21 12:51:23 -0300

    added function to add and access the batch database

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 from datetime import datetime, timedelta
 
+from prettytable import PrettyTable
+
+# initiate the prettytable
+x = PrettyTable()
+
 # initiate the app
 app = Flask(__name__)
 
@@ -21,16 +26,66 @@ class Batch(db.Model):
 
     """
     id = db.Column(db.Integer, primary_key=True)
-    coffee_name = db.Column(db.String(100), nullable=False )
+    coffee_name = db.Column(db.String(100), nullable=False)
     batch_type = db.Column(db.String(50), nullable=False)
     batch_start_water_volume = db.Column(db.Float, nullable=False)
-    batch_end_water_volume = db.Column(db.Float, nullable=False)
+    batch_end_water_volume = db.Column(db.Float, nullable=True)
     batch_start_water_temperature = db.Column(db.Float, nullable=False)
-    batch_end_water_temperature = db.Column(db.Float, nullable=False)
+    batch_end_water_temperature = db.Column(db.Float, nullable=True)
     batch_coffee_quantity = db.Column(db.Float, nullable=False)
     batch_start_date = db.Column(db.DateTime)
     batch_estimated_end_date = db.Column(db.DateTime)
     batch_actual_end_date = db.Column(db.DateTime)
-    Batch_expiration_date = db.Column(db.DateTime)
-    batch_ph = db.Column(db.Float, nullable=False)
+    batch_expiration_date = db.Column(db.DateTime)
+    batch_ph = db.Column(db.Float, nullable=True)
 
+def new_batch(coffee_name: str,
+              batch_type: str,
+              batch_start_water_volume: float,
+              batch_start_water_temperature: float,
+              batch_coffee_quantity: float) -> None:
+    """ Adds a new batch to the date base."""
+
+    batch = Batch(coffee_name=coffee_name,
+                  batch_type=batch_type,
+                  batch_start_water_volume=batch_start_water_volume,
+                  batch_start_water_temperature=batch_start_water_temperature,
+                  batch_coffee_quantity=batch_coffee_quantity)
+    
+    db.session.add(batch)
+    db.session.commit()
+
+def get_batch_by_id(batch_id: int) -> None:
+    batch = Batch.query.filter_by(id=batch_id).first()
+    x.align ='r'
+    x.field_names = ['id',
+                     'coffee_name',
+                     'batch_type',
+                     'batch_start_water_volume',
+                     'batch_end_water_volume',
+                     'batch_start_water_temperature',
+                     'batch_end_water_temperature',
+                     'batch_coffee_quantity',
+                     'batch_start_date',
+                     'batch_estimated_end_date',
+                     'batch_actual_end_date',
+                     'batch_expiration_date',
+                     'batch_ph'
+                     ]
+    x.add_row(
+        [batch.id,
+         batch.coffee_name,
+         batch.batch_type,
+         batch.batch_start_water_volume,
+         batch.batch_end_water_volume,
+         batch.batch_start_water_temperature,
+         batch.batch_end_water_temperature,
+         batch.batch_coffee_quantity,
+         batch.batch_start_date,
+         batch.batch_estimated_end_date,
+         batch.batch_actual_end_date,
+         batch.batch_expiration_date,
+         batch.batch_ph]
+         )
+    print(x)
+    
